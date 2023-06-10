3235f050a17b9475926ee3a5b24f07d64d6ec7e4
yaniarani <115916449+yarauzz@users.noreply.github.com>
2023-06-10 11:20:30 +0700

    fix

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\Contracts\HasApiTokens;
 
 class ApiAuthController extends Controller
 {
